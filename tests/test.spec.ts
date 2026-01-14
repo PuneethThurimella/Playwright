b650e21af6c7b91e20ed1880c6993f5cdaee1420
PuneethThurimella <puneeth4800@gmail.com>
2026-01-14 20:15:50 +0530

    setup POM, fixture

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -2,5 +2,5 @@ import { test } from '../fixtures/fixture';
 
 test('Sample test', { tag: ['@smoke', '@regression'] }, async ({page, loginPage }) => {
     await page.goto('https://www.amazon.in');    
-    await loginPage.login();
+    await loginPage.login('Admin', 'Admin123');
 });
