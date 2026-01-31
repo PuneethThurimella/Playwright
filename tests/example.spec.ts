d15687addb5343d403225e134e2b291fc150d428
PuneethThurimella <puneeth4800@gmail.com>
2026-01-31 08:50:56 +0530

    playwright

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("Sample UI Test", { tag: ["@smoke", "@regression"] }, async ({ page }) => {
+test("Sample Test", async ({ page }) => {
   await page.goto("https://playwright.dev/");
   await expect(page).toHaveTitle(/Playwright/);
   await page.getByRole("link", { name: "Get started" }).click();
