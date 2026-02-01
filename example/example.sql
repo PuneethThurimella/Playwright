8bf54eb327ba7d46d354d43f277a4b6ef7e259d9
PuneethThurimella <puneeth4800@gmail.com>
2026-02-01 18:38:00 +0530

    playwright

--- example/example.sql
+++ example/example.sql
@@ -30,6 +30,25 @@ SELECT * FROM Employee WHERE HireDate IN (
     (SELECT MAX(HireDate) FROM Employee)
 );
 
+--Insert a single row
+INSERT INTO employees (id, name, department, salary)
+VALUES (1, 'John Doe', 'Engineering', 75000);
+
+--Insert multiple rows
+INSERT INTO employees (id, name, department, salary)
+VALUES (2, 'Jane Smith', 'HR', 60000), (3, 'Mike Brown', 'Finance', 68000);
+
+--Insert using values from another table
+INSERT INTO archived_employees (id, name, department)
+SELECT id, name, department FROM employees WHERE department = 'HR';
+
+--Update a single column
+UPDATE employees SET salary = 80000 WHERE id = 1;
+
+--Update multiple columns
+UPDATE employees SET department = 'Product', salary = 82000 WHERE name = 'John Doe';
+
+
 
 
 
--- example/yatra.spec.ts
+++ example/yatra.spec.ts
@@ -1,5 +1,10 @@
 import { test, expect } from "@playwright/test";
 
 test("yatra", { tag: ["@smoke"] }, async ({ page }) => {
-  await page.goto("url");
+  await page.goto("https://www.yatra.com/");
+  const cross = page.locator("//section//img[@alt='cross']");
+  if (await cross.isVisible()) {
+    await cross.click();
+    await page.pause();
+  }
 });
