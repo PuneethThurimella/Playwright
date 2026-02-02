c974083af65ceeb81f5c6842c7e1dadb75db3fd9
PuneethThurimella <puneeth4800@gmail.com>
2026-02-02 11:00:47 +0530

    playwright

--- example/wipro.spec.ts
+++ example/wipro.spec.ts
@@ -2,4 +2,8 @@ import { test, expect } from "@playwright/test";
 
 test("Wipro", async ({ page }) => {
   await page.goto("https://trytestingthis.netlify.app/");
+  await page.getByLabel('First name').fill('Admin');
+  await page.getByLabel('Last name').fill('password');
+  await page.locator('#male').click();
+  await page.pause();
 });
--- example/yatra.spec.ts
+++ example/yatra.spec.ts
@@ -2,10 +2,13 @@ import { test, expect } from "@playwright/test";
 
 test("yatra", { tag: ["@smoke"] }, async ({ page }) => {
   await page.goto("https://www.yatra.com/");
-  const cross = page.locator("//section//img[@alt='cross']");
+  const cross = page.locator("section img[alt='cross']");
   await cross.waitFor({ state: "visible", timeout: 5000 }).catch(() => {});
   if (await cross.isVisible()) {
     await cross.click();
-    await page.pause();
   }
+  await page.locator("p[title='New Delhi']").click();
+  await page.locator("#input-with-icon-adornment").fill("Bangalore");
+  await page.pause();
+
 });
